a80880a2fbc0e31086a842387b1249d87173fd84
coder-mvk <vamsi8136@gmail.com>
2025-03-04 06:30:53 +0530

    learn mapping method in react js

--- learning-mapmethod/src/App.jsx
+++ learning-mapmethod/src/App.jsx
@@ -1,17 +1,18 @@
 
 function App() {
-  // const Heroes = ['IRONMAN', 'THOR', 'CAPTAIN AMERICA', 'HULK', 'VISION', 'BLACK PANTHER']
-  const Heroes = []
-  // const emptyMessage = Heroes.length === 0 ? <h3>Still thinging</h3> : null;
+  // const stdLst = ["Soumya", "Vedant", "Ashwin", "Lanchana", "Jayesh","Jai", "Neelesh","Neil",]
+  const stdLst = []
+
+  if (stdLst == 0){
+    return <h3>List is empty</h3>
+  }
+
   return <>
-  <h1>Super Heroes List</h1>
-  {Heroes.length === 0 && <h3>Still thinking</h3>}
+  <h1>Student List</h1>
   <ul className="list-group">
-  {Heroes.map((hero, index)=>(<li key={index} className="list-group-item">{hero}</li>))}
+    {stdLst.map((std,index) => (<li key={index} className="list-group-item">{std}</li>))}
   </ul>
-  
   </>
-  
 }
 
 export default App
